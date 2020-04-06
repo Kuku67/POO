8d521ed1ce632f1dd57bf93dc36547b1499cb756
Kuku67 <62292732+Kuku67@users.noreply.github.com>
2020-04-06 15:28:09 +0200

    Update app.js

--- PHP_Pendu/app.js
+++ PHP_Pendu/app.js
@@ -1,14 +1,23 @@
+$(document).ready(function(){
+    document.cookie = "difficulty=easy";
+    document.cookie = "theme=animals";
+    $(".animals").css({'background-color' : 'rgb(196, 35, 35)', 'color' : 'white'});
+    $(".easy").css({'background-color' : 'rgb(196, 35, 35)', 'color' : 'white'});
+});
+
 $("#difficulty a").click(function(){
     var valueDiff = $(this).attr('class');
     var cookieDiff = "difficulty";
     document.cookie = cookieDiff + "=" + valueDiff;
 
 });
+
 $("#theme a").click(function(){
     var valueTheme = $(this).attr('class');
     var cookieTheme = "theme";
     document.cookie = cookieTheme + "=" + valueTheme;
 });
+
 $("#parameters a").click(function(){
     $(this).css({'background-color' : 'rgb(196, 35, 35)', 'color' : 'white'});
     $(this).siblings('a').css({'background-color' : 'rgb(223, 205, 51)', 'color' : 'black'});
